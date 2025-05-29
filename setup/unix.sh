b4faf84a245d34d67aae02a14aafdc255fa6e956
jereidk <95512894+jereidk@users.noreply.github.com>
2025-05-29 10:19:55 -0500

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -3,15 +3,15 @@
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download
 cd ..
-echo Making the main haxelib and setuping folder in same time..
-mkdir -p ~/haxelib && haxelib setup ~/haxelib
+echo Makking the main haxelib and setuping folder in same time..
+mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
 haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib install openfl 9.4.1 --quiet
 haxelib git flixel https://github.com/kittycathy233/flixel --quiet
-haxelib install flixel-addons 2.11.0 --quiet
+haxelib install flixel-addons 3.3.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
